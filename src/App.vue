047fa1bd8eafd59403d1b0d282e67b8958a89784
Liess inoudjal <liess.inoudjal@inetum.com>
2021-11-15 10:30:02 +0100

    add navLink

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavLink from './components/NavLink.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		NavLink
+	}
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+	<div id="app">
+		<nav>
+			<NavLink url="/" text="Accueil" />
+			<NavLink url="/about" text="À propos" />
+			<NavLink url="/contact" text="Contact" />
+		</nav>
+
+	</div>
+</template>
